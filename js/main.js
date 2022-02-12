10d882eb2a375b6bcd496b7eaac1f1ebc72a4fca
Luis Lopez <lueunbii98@gmail.com>
2022-02-12 06:12:29 -0500

    calculate function refactored

--- js/main.js
+++ js/main.js
@@ -108,20 +108,10 @@ keys.addEventListener('click', e => { //Attaches an event handler to the element
 
 //Function to calculate the result
 const calculate = (n1, operator, n2) => {
-    let result = ''
-
-    if (operator === 'add'){
-        result = parseFloat(n1) + parseFloat(n2)
-    }
-    else if (operator === 'subtract'){
-        result = parseFloat(n1) - parseFloat(n2)
-    }
-    else if (operator === 'multiply'){
-        result = parseFloat(n1) * parseFloat(n2)
-    }
-    else if (operator === 'divide'){
-        result = parseFloat(n1) / parseFloat(n2)
-    }
-
-    return result
+    const firstNum = parseFloat(n1)
+    const secondNum = parseFloat(n2)
+    if (operator === 'add') return firstNum + secondNum
+    if (operator === 'subtract') return firstNum - secondNum
+    if (operator === 'multiply') return firstNum * secondNum
+    if (operator === 'divide') return firstNum / secondNum
 }
